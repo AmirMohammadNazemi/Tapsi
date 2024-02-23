34b3e831a16ecb6f5ac2ea7a166c6d135cb70684
Amir Mohammad Nazemi <amir.mo.9696@gmail.com>
2024-02-23 17:04:39 +0330

    the final version of tapsi project on 2024/02/23

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// -------------------------------------------
+// -------------------------------------------
+// ------------ NAV BAR TOGGLER --------------
+// -------------------------------------------
+// -------------------------------------------
+// const toggler = document.querySelector(".nav__toggler");
+// const navbar = document.querySelector(".nav");
+// toggler.addEventListener("click", (e) => {
+// 	console.log("clicked");
+// 	navbar.classList.toggle("nav__expanded");
+// });
+
+// -------------------------------------------
+// -------------------------------------------
+// ------------ SERVICES SECTION -------------
+// -------------------------------------------
+// -------------------------------------------
+// const tabs = document.querySelectorAll(".tab");
+// const tabContents = document.querySelectorAll(".tab-content");
+// tabs.forEach((tab) => {
+// 	tab.addEventListener("click", (e) => {
+// 		const targetTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+
+// 		// REMOVE PREVIOUS ACTIVED CLASSES
+// 		tabs.forEach((tab) => tab.classList.remove("active"));
+// 		tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+
+// 		// ADD NEW ACTIVE CLASSES
+// 		tab.classList.add("active");
+// 		targetTabContent.classList.add("active");
+// 	});
+// });
+
+// --------------------------------------------
+// --------------------------------------------
+// ------------ ACCORDION SECTION -------------
+// --------------------------------------------
+// --------------------------------------------
+// this script is not working perfectly, but it's ok for now. after learning js course it becomes as a piece of cake.
+// --------------------------------------------
+// const accordionHeader = document.querySelector(".accordion__header");
+// const accordion = document.querySelector(".accordion");
+// accordionHeader.addEventListener("click", (e) => {
+// 	console.log("clicked");
+// 	accordion.classList.toggle("accordion__expanded");
+// });
